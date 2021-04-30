3862d97a904853cb9badf4070e10eb09d4a55570
Stuart Knightley <knightle@adobe.com>
2021-04-30 13:29:29 -0700

    Simplify repro a bit

--- 2-test.js
+++ 2-test.js
@@ -1,5 +1,6 @@
-// Rename this file to 0-test.js for coverage to work
-import { Bad } from "./bad.js";
+// Rename this file to 0-test.js for coverage of B to work
+// import { Bad } from "./bad.js";
+import { Bad } from "./index.js";
 
 it("calls Bads's method", () => {
   const bad = new Bad();
